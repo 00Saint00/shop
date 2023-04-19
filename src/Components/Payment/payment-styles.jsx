0f3ad0d72c1f79c8f63f2857e3788b0b993d548d
00Saint00 <paulotomewo3@gmail.com>
2023-04-19 01:03:27 +0100

    payment fixed

--- src/Components/Payment/payment-styles.jsx
+++ src/Components/Payment/payment-styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Button from "../Button/Button";
 
 export const PaymentContainer = styled.div`
   height: 300px;
@@ -11,4 +12,9 @@ export const PaymentContainer = styled.div`
 export const FormContainer = styled.form`
   height: 100px;
   min-weight: 500px;
+`;
+
+export const PaymentButton = styled(Button)`
+  margin-left: auto;
+  margin-top: 30px;
 `;
